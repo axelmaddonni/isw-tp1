bc6e703528f1ac1ce149d2b0c507317d2f9c1ca2
gciruelos <gonzalo.ciruelos@gmail.com>
2016-09-08 02:13:22 -0300

    busqueda de bares hecha

---
+++ src/app/bares.py
@@ -0,0 +1,78 @@
+from math import sqrt
+import googlemaps
+from datetime import datetime
+
+# Como usar: https://developers.google.com/maps/web-services/?hl=es
+# Mas: https://github.com/googlemaps/google-maps-services-python
+# Por favor no distribuir ni usar para otra cosa esta clave, porque es mia y
+# su uso indiscriminado puede hacer que no pueda usar mas la api de gmaps :(
+# max de requests por dia: 5000
+# Si algo les da error que no anda, avisenme.
+# Para sacar una clave para lo que quieran:
+#    https://console.developers.google.com/?hl=es-419
+GOOGLE_MAPS_KEY = "AIzaSyBN8fuLr8PXqvxNrZ_WtcIhqo9K5XKqWDw"
+gmaps = googlemaps.Client(key=GOOGLE_MAPS_KEY)
+
+class Direccion:
+    global gmaps
+    def __init__(self, direccion):
+        self.__direccion = direccion
+        self.__latlong = (0, 0)
+        result = gmaps.geocode(direccion)
+        try:
+            res = result[0]['geometry']['location']
+            self.__latlong = (res["lat"], res["lng"])
+        except:
+            pass
+    def direccion(self):
+        return self.__direccion
+    def latlong(self):
+        return self.__latlong
+    def distanciaMetros(self, direccion2):
+        distancia = 10000000
+        try:
+            resultado = gmaps.distance_matrix([self.__latlong], [direccion2.latlong()], units="metric")
+            distancia = resultado["rows"][0]["elements"][0]["distance"]["value"]
+        except:
+            pass
+        return distancia
+
+
+class Bar:
+    def __init__(self, nombre, direccion):
+        self.__nombre = nombre
+        self.__direccion = direccion
+        self.__denios = []
+    def nombre(self):
+        return self.__nombre
+    def direccion(self):
+        return self.__direccion
+    def editarDireccion(nuevaDireccion):
+        self.__direccion = nuevaDireccion
+    def agregarDuenio(nuevoDuenio):
+        self.__duenios.append(nuevoDuenio)
+
+class PerfilDeBar:
+    def __init__(self, bar):
+        self.elBar = bar
+        self.votos = {}
+        self.comentarios = []
+    def bar(self):
+        return self.elBar
+
+class BuscadorDeBares:
+    def __init__(self, bares):
+        self.losBares = bares
+    def buscar(self, dir_usuario):
+        baresCercanos = []
+        for bar in self.losBares:
+            if bar.direccion().distanciaMetros(dir_usuario) <= 400:
+                baresCercanos.append(PerfilDeBar(bar))
+        return baresCercanos
+
+
+bar1 = Bar('Mumbai', Direccion('Honduras 5684, CABA, Argentina'))
+bar2 = Bar('Niceto', Direccion('Av Cnel. Niceto Vega 5510, CABA, Argentina'))
+buscador = BuscadorDeBares([bar1, bar2])
+
+
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app
+from app.bares import Direccion, Bar, BuscadorDeBares, buscador
 import math
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,4 +13,12 @@ def index():
 @app.route('/raiz_de_dos')
 def raiz():
     return str(math.sqrt(2.0))
+
+@app.route('/resultados/<posicion>')
+def resultados_busqueda(posicion):
+    global buscador
+    posicion_del_usuario = Direccion(posicion)
+    return render_template('resultados_busqueda.html',
+                           bares=buscador.buscar(posicion_del_usuario),
+                           dirusuario=posicion_del_usuario)
 
