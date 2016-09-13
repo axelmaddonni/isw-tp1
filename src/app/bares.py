e05000e1511b8f5cae541f50437eca52fbd2e820
manucosta <manucos94@gmail.com>
2016-09-13 14:01:42 -0300

    Agregada clase BaseDeDatosDeBares

--- src/app/bares.py
+++ src/app/bares.py
@@ -14,56 +14,78 @@ GOOGLE_MAPS_KEY = "AIzaSyBN8fuLr8PXqvxNrZ_WtcIhqo9K5XKqWDw"
 gmaps = googlemaps.Client(key=GOOGLE_MAPS_KEY)
 
 class Direccion:
-    global gmaps
-    def __init__(self, direccion):
-        self.__direccion = direccion
-        self.__latlong = (0, 0)
-        try:
-            result = gmaps.geocode(direccion)
-            res = result[0]['geometry']['location']
-            self.__latlong = (res["lat"], res["lng"])
-        except:
-            pass
-    def direccion(self):
-        return self.__direccion
-    def latlong(self):
-        return self.__latlong
-        return distancia
+  global gmaps
+  def __init__(self, direccion):
+    self.__direccion = direccion
+    self.__latlong = (0, 0)
+    try:
+      result = gmaps.geocode(direccion)
+      res = result[0]['geometry']['location']
+      self.__latlong = (res["lat"], res["lng"])
+    except:
+      pass
+  def direccion(self):
+    return self.__direccion
+  def latlong(self):
+    return self.__latlong
+    return distancia
 
 class Bar:
-    def __init__(self, nombre, direccion):
-        self.__nombre = nombre
-        self.__direccion = direccion
-        self.__duenios = []
-    def nombre(self):
-        return self.__nombre
-    def direccion(self):
-        return self.__direccion
-    def editarDireccion(nuevaDireccion):
-        self.__direccion = nuevaDireccion
-    def agregarDuenio(nuevoDuenio):
-        self.__duenios.append(nuevoDuenio)
+  def __init__(self, nombre, direccion):
+    self.__nombre = nombre
+    self.__direccion = direccion
+    self.__duenios = []
+  def nombre(self):
+    return self.__nombre
+  def direccion(self):
+    return self.__direccion
+  def editarDireccion(nuevaDireccion):
+    self.__direccion = nuevaDireccion
+  def agregarDuenio(nuevoDuenio):
+    self.__duenios.append(nuevoDuenio)
 
 class PerfilDeBar:
-    def __init__(self, bar):
-        self.elBar = bar
-        self.votos = {}
-        self.comentarios = []
-    def bar(self):
-        return self.elBar
+  def __init__(self, bar):
+    self.elBar = bar
+    self.votos = {}
+    self.comentarios = []
+  def bar(self):
+    return self.elBar
 
 class BuscadorDeBares:
-    def __init__(self, bares):
-        self.losBares = bares
-    def buscar(self, dir_usuario):
-        direccionesDeLosBares = [bar.direccion().direccion() for bar in self.losBares]
-        try:
-            resultado = gmaps.distance_matrix(dir_usuario.direccion(), direccionesDeLosBares, units="metric", mode = "walking")
-            return [PerfilDeBar(self.losBares[i]) for i in range(len(self.losBares)) if resultado["rows"][0]["elements"][i]["distance"]["value"] <= 400]
-        except:
-            pass
+  def __init__(self, bares):
+    self.BBDDBares = BaseDeDatosDeBares(bares)
+  def agregarALaBBDD(self, bares):
+    self.BBDDBares.agregarBares(bares)
+  def removerDeLaBBDD(self, bar):
+    self.BBDDBares.borrarBar(bar)
+  def buscar(self, dir_usuario):
+    direccionesDeLosBares = list(self.BBDDBares.direcciones())
+    losBares = list(self.BBDDBares.bares())
+    try:
+      resultado = gmaps.distance_matrix(dir_usuario.direccion(), direccionesDeLosBares, units="metric", mode = "walking")
+      return [PerfilDeBar(losBares[i]) for i in range(len(losBares)) if resultado["rows"][0]["elements"][i]["distance"]["value"] <= 400]
+    except:
+      pass
 
+class BaseDeDatosDeBares:
+  def __init__(self, bares):
+    self.losBaresPorDir = dict([(bar.direccion().direccion(), bar) for bar in bares])
+  def agregarBares(self, bares):
+    for bar in bares:
+      key = bar.direccion().direccion()
+      self.losBaresPorDir[key] = bar   
+  def borrarBar(self, bar):
+    key = bar.direccion().direccion()
+    if key in self.losBaresPorDir:
+      del self.losBaresPorDir[key]
+  def direcciones(self):
+    return self.losBaresPorDir.keys()
+  def bares(self):
+    return self.losBaresPorDir.values()
 
 bar1 = Bar('Mumbai', Direccion('Honduras 5684, CABA, Argentina'))
 bar2 = Bar('Niceto', Direccion('Av Cnel. Niceto Vega 5510, CABA, Argentina'))
+bar3 = Bar('Bouquet', Direccion('Av Cabildo 1400, CABA, Argentina'))
 buscador = BuscadorDeBares([bar1, bar2])
+buscador.agregarALaBBDD([bar3])
