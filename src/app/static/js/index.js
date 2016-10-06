feb0eeeae4e0224fc26e71f389e35bfb1ae900c3
Axel Maddonni <axel.maddonni@gmail.com>
2016-10-06 03:09:58 -0300

    Vista de Bar con comentarios y funciones para valorar bares.

--- src/app/static/js/index.js
+++ src/app/static/js/index.js
@@ -1,3 +1,33 @@
-$('.info dd').each(function() {
-  $(this).css({width: $(this).text()+'%'});
-});
+$(document).ready(function(){ 
+	$('.info dd').each(function() {
+	  $(this).css({width: $(this).text()+'%'});
+	});
+
+
+    setTimeout(function() {
+		$('#cover').css({
+	    "webkitTransform":"translateX(10%)",
+	    "border-color":"#00a7c9",
+		});
+
+		$('#calificaciones').css({
+	    "webkitTransform":"translateX(80%)",
+	    "width": '100%'
+		});
+
+		$('#comentar').show( "fast", function() {
+		});
+
+		$('#verComentarios').show( "fast", function() {
+		});
+
+		$('#camino').show( "fast", function() {
+		});
+
+    }, 100);
+
+ })
+
+
+
+
